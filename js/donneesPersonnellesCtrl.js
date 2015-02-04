55238a7e99ae585f720a9329446808eb15f772b2
unknown <Elliott.CHIARADIA@SC-C333-PC09.sc.cpnv.ch>
2015-02-04 08:10:53 +0100

    s

--- js/donneesPersonnellesCtrl.js
+++ js/donneesPersonnellesCtrl.js
@@ -1,21 +1,18 @@
 countryControllers.controller('donneesPersonnellesCtrl', ['$scope', '$routeParams', '$http',
     function($scope, $routeParams, $http) {
-        $http.get(apiPath+'/users/1?token=asdf').success(function(data) {
-            console.log(data);
-            $scope.user = data;
+$http.get(apiPath+'/users/1?token=asdf').success(function(data) {
+    console.log(data);
+    $scope.user = data;
 
-        });
-        $scope.submit = function () {
-            alert('coucou');
-            $http.put(apiPath+'/users/1?token=asdf',
-                $scope.user).success(function(data) {
-                console.log($scope.user);
-                 //= data;
-                    alert('coucou2');
-
-            });
-        };
-    }]);
+});
+$scope.submit = function () {
+    $http.put(apiPath+'/users/1?token=asdf', $scope.user).success(function(data) {
+        alert("Vous données ont été mises à jour avec succès !");
+    }).error(function(error){
+        alert("Une erreur est survenue. Veuillez réessayer plus tard.")
+    });
+};
+}]);
 
 
 
